ea3cf025b01cb41715f60394bfc53751f411c624
pimpaoz15 <120665865+pimpaoz15@users.noreply.github.com>
2023-06-02 17:29:49 +0100

    Exercise 4 small changes and added a few comments

--- pergunta_4/P4.R
+++ pergunta_4/P4.R
@@ -1,10 +1,19 @@
+# set seed
 set.seed(1275)
-amostra <- rexp(2493, rate=12.5)
+
+# generate sample
+amostra <- rexp(2493, rate = 12.5)
+
+# set parameters
 sj <- cumsum(amostra)
 T <- ceiling(sj[2493])
-subintervalos <- seq(1, T, by=1)
-contagem <- table(cut(sj, breaks=subintervalos, right=FALSE))
+
+# set unitary subintervals
+subintervalos <- seq(1, T, by = 1)
+contagem <- table(cut(sj, breaks = subintervalos, right = FALSE)) # right = FALSE argument specifies that the intervals should be left-closed and right-open.
 media_obs <- mean(contagem)
-media_teorica <- length(sj) / T
-desvio_absoluto <- abs(media_obs - media_teorica)
+desvio_absoluto <- abs(media_obs - 12.5)
 desvio_absoluto <- round(desvio_absoluto, 4)
+
+# print result
+print(desvio_absoluto)
