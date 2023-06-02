ac2ff68774e991b485e1bc5ce6e12a027272c3ea
pimpaoz15 <120665865+pimpaoz15@users.noreply.github.com>
2023-06-02 17:31:59 +0100

    Changed variables names to be more descriptive

--- pergunta_4/P4.R
+++ pergunta_4/P4.R
@@ -1,19 +1,19 @@
-# set seed
+# Set seed
 set.seed(1275)
 
-# generate sample
-amostra <- rexp(2493, rate = 12.5)
+# Generate sample
+sample <- rexp(2493, rate = 12.5)
 
-# set parameters
-sj <- cumsum(amostra)
+# Set parameters
+sj <- cumsum(sample)
 T <- ceiling(sj[2493])
 
-# set unitary subintervals
-subintervalos <- seq(1, T, by = 1)
-contagem <- table(cut(sj, breaks = subintervalos, right = FALSE)) # right = FALSE argument specifies that the intervals should be left-closed and right-open.
-media_obs <- mean(contagem)
-desvio_absoluto <- abs(media_obs - 12.5)
-desvio_absoluto <- round(desvio_absoluto, 4)
+# Set unitary subintervals
+subintervals <- seq(1, T, by = 1)
+count <- table(cut(sj, breaks = subintervals, right = FALSE)) # right = FALSE argument specifies that the intervals should be left-closed and right-open.
+obs_mean <- mean(count)
+absolute_deviation <- abs(obs_mean - 12.5)
+absolute_deviation <- round(absolute_deviation, 4)
 
-# print result
-print(desvio_absoluto)
+# Print result
+print(absolute_deviation)
