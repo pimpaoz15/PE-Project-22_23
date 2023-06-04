e708784914f9c76616c402174fd7bee32f9daf6a
pimpaoz15 <120665865+pimpaoz15@users.noreply.github.com>
2023-06-04 12:38:55 +0100

    Exercicio 4 na duvida

--- pergunta_4/P4.R
+++ pergunta_4/P4.R
@@ -17,3 +17,32 @@ absolute_deviation <- round(absolute_deviation, 4)
 
 # Print result
 print(absolute_deviation)
+
+
+
+# OU será assim
+
+# set seed
+set.seed(1275)
+
+# generate sample
+amostra <- rexp(2493, rate = 12.5)
+
+# set parameters
+sj <- cumsum(amostra)
+T <- ceiling(sj[2493])
+
+# set unitary subintervals
+subintervalos <- seq(1, T, by = 1)
+contagem <- table(cut(sj, breaks = subintervalos, right = FALSE)) # right = FALSE argument specifies that the intervals should be left-closed and right-open.
+media_obs <- mean(contagem)
+media_teorica <- length(sj) / T
+desvio_absoluto <- abs(media_obs - media_teorica)
+desvio_absoluto <- round(desvio_absoluto, 4)
+
+# print result
+print(desvio_absoluto)
+
+
+
+
