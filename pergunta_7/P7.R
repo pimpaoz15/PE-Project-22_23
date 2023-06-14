c3d2a296ffc1a5a492e2cf2be54b03bf0903f7df
pimpaoz15 <120665865+pimpaoz15@users.noreply.github.com>
2023-06-14 21:55:38 +0100

    Final

--- pergunta_7/P7.R
+++ pergunta_7/P7.R
@@ -1,7 +1,8 @@
-set.seed(1114) # Fixar a semente
-m <- 1248
-n <- 10
-p <- 0.47
+# Dados do problema
+set.seed(1065) # Fixar a semente
+m <- 1507
+n <- 18
+p <- 0.31
 
 # amostra
 X <- matrix(rnorm(m * n, 0, 1), nrow = m, ncol = n)
@@ -10,15 +11,15 @@ X <- matrix(rnorm(m * n, 0, 1), nrow = m, ncol = n)
 Y <- rep(0, m)
 for (i in 1:m) {
     for (j in 1:n) {
-        Y[i] <- Y[i] + X[i, j]
+        Y[i] <- Y[i] + X[i, j]^2
     }
 }
 
 # diferença entre o quantil de probabilidade 0.47 e
 # quantil correspondente à distribuição teórica da soma de quadrados de variáveis normais reduzidas independentes
 
-quantil_047 <- quantile(Y, probs = p, type = 2) # quantil de probabilidade 0.47
-quantil <- qchisq(p, df = 1, ncp = 0)
-Resultado <- quantil_047 - quantil
+quantil_Y <- quantile(Y, probs = p, type = 2) # quantil de probabilidade p
+quantil_teorica <- qchisq(p, df = n) # quantil de probabilidade p teorico
+Resultado <- quantil_Y - quantil_teorica
 Resultado <- round(Resultado, digits = 4)
-print(Resultado)
+print(paste("Resultado:", abs(Resultado)))
