11b143e0a7714018ad37e5dc3a0e35f12fa58cbf
pimpaoz15 <120665865+pimpaoz15@users.noreply.github.com>
2023-06-17 18:26:52 +0100

    .

---
+++ pergunta_10/p.R
@@ -0,0 +1,21 @@
+set.seed(131)
+m <- 300
+n <- 45
+alfa <- 0.02
+c <- 1.645
+
+total <- 0
+samples <- replicate(m, {
+    a <- rnorm(n, 38.7, 2)
+})
+
+for (i in 1:m) {
+    b <- samples[1:n, i]
+    z_ho <- (mean(b) - 37.5) / (2 / sqrt(n))
+
+    if ((z_ho > (-c)) & (z_ho < c)) {
+        total <- total + 1
+    }
+}
+probability <- total / m
+round(probability, 3)
